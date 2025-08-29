24a3bfc3529159f0c3bb99c8e8279906cfa7d472
Guilherme Felipe <guimota_silver@hotmail.com>
2025-08-29 15:02:46 -0300

    Subindo criptografia das senhas de usuário

--- backend/src/routes/usuario.js
+++ backend/src/routes/usuario.js
@@ -1,4 +1,5 @@
 import { supabase } from '../lib/supabaseClient.js'
+import bcrypt from "bcryptjs";
 
 // GET
 export async function listarUsuarios(req, reply) {
@@ -24,18 +25,35 @@ export async function login(req, reply) {
   const { data, error } = await supabase
     .from('tb_usuario')
     .select('id_usuario, tb_usuario_email, tb_usuario_senha, tb_usuario_nome, tb_usuario_admin')
-    .eq('tb_usuario_email', email)
-    .eq('tb_usuario_senha', senha)
+    .eq('tb_usuario_email', email);
+
   if (error) return reply.code(500).send({ error: error.message })
+  if (!data || data.length === 0) return reply.code(401).send({ success: false, message: "Credenciais inválidas." });
+
+  const usuario = data[0];
+  console.log('usuario', usuario);
+
+  const ok = await bcrypt.compare(senha ?? "", usuario.tb_usuario_senha);
+  if (!ok) return reply.code(401).send({ success: false, message: "Credenciais inválidas." });
+
+  const usuarioRetorno = {
+    id: usuario.id_usuario,
+    nome: usuario.tb_usuario_nome,
+    admin: usuario.tb_usuario_admin
+  }
+
   return { success: true, 
-    data: data[0], 
+    data: usuarioRetorno, 
     message: "Login efetuado com sucesso." }
 }
 
 // POST
 export async function criarUsuario(req, reply) {
+  const senhaHash = await bcrypt.hash(req.body.tb_usuario_senha ?? "", 12);
+
   const usuario = {
     ...req.body,
+    tb_usuario_senha: senhaHash,
     tb_usuario_inativo: false
   }
   const { data, error } = await supabase
